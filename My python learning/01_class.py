158689752aff33200e7be41a29c010f50db20e8b
Urina <rajbhandariurina@gmail.com>
2026-09-02 19:29:50 +0545

    learning

---
+++ My python learning/01_class.py
@@ -0,0 +1,9 @@
+First_Name=input("Enter your first name: ")
+Last_Name=input("Enter your last name: ")
+Address=input("Enter your address: ")
+age=input("Enter your age: ") #storing age 
+
+print()
+print("=" * 30)
+print(f"Your name is {First_Name} {Last_Name}, you live at {Address}, and you are {age} years old.")
+print("=" * 30)
